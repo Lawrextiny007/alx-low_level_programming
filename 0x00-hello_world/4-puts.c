659daac06b2e60578142978a23b96dfebe344db7
Lawrextiny007 <Lawrencejimmy884@gmail.com>
2022-12-08 16:02:55 -0800

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"programming is like biulding a multi lingual puzzle");
+	puts("\"programming is like biulding a multilingual puzzle");
 	return (0);
 }
 
