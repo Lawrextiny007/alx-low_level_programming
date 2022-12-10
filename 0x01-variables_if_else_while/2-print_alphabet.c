8e34f9972d9eb98754ff85c20be5cc15ff430522
Lawrextiny007 <Lawrencejimmy884@gmail.com>
2022-12-10 03:26:32 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
