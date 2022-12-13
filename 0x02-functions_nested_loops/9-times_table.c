0fa9c0d7ca63fac0066cd569e3f4c47e79f59643
Lawrextiny007 <Lawrencejimmy884@gmail.com>
2022-12-13 15:22:52 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			c = a + b;
+			c = a * b;
 			if ((c / 10) == 0)
 			{
 				if (b == 0)
@@ -25,7 +25,7 @@ void times_table(void)
 					_putchar(' ');
 					_putchar((c % 10) + '0');
 				}
-				if (b < 0)
+				if (b < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
