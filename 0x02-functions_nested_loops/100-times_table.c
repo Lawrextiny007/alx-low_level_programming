b927be2aee7cd2bf295acf20250b93464e05d0f5
Lawrextiny007 <Lawrencejimmy884@gmail.com>
2022-12-14 15:25:13 -0800

    task12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,12 +6,13 @@
  * Return:void
  */
 
-void print_times_table(int n)
+void print_times_table(int n);
 {
 int a = 0, rep, b;
 
 if (n < 0 || n > 15)
 	return;
+
 while (a <= n)
 {
 	for (b = 0; b <= n; b++)
@@ -46,4 +47,5 @@ while (a <= n)
 	_putchar('\n');
 	a++;
 }
-}
+
+{
