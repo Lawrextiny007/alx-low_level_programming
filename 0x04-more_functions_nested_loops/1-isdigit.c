4d96e66c2cf6e354e5566c52cbc2c5f0ea16b1df
Lawrextiny007 <Lawrencejimmy884@gmail.com>
2022-12-15 09:26:24 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isdigit - checks if a passed character is a digit from 0 through 9  or not
+ * Return: 1
+ */
+
+int _isdigit(int c)
+{
+	char c;
+	
+       	c = '0';
+
+	if (isdigit(c))
+		putchar(c);
+	return (1);
+	
+	else (c = 'a')
+	return (0);
+}
+
+
+	
+
