e83788512add1c126edc8ed58d09e963ea45eb14
Lawrextiny007 <Lawrencejimmy884@gmail.com>
2022-12-15 13:57:33 -0800

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,21 +11,23 @@
 
 int main(void)
 {
-int i;
+	for (int i = 1; i <= 100; ++i)
+	{
+		if (!(i % 15))
+		{
+			printf("%d FizzBuzz\n", i);
+		} else if (!(i % 3))
 
-for (i = 1; i <= 100; i++)
-{
-if ((i % 3 == 0) && (i % 5 == 0))
-printf("FizzBuzz");
-else if (i % 3 == 0 && i % 5 != 0)
-printf("Fizz");
-else if (i % 5 == 0 && i % 3 != 0)
-printf("Buzz");
-else if (i == 1)
-printf("%d", i);
-else
-printf(" %d", i);
-}
-printf("%d", '\n');
-return (0);
+		{
+			printf("%d Fizz\n", i);
+		} else if (!(i % 5))
+
+		{
+			printf("%d Buzz\n", i);
+		} else
+
+		{
+			printf("%d\n", i);
+		} return (0);
+	}
 }
