ab1d99680b74062d1205bf977411cc2805769831
Lawrextiny007 <Lawrencejimmy884@gmail.com>
2022-12-30 06:15:56 -0800

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,6 @@
 /**
  * print_rev - function that prints a string in reverse
  * @s: character to be printed in reverse
- * Return: 0
  */
 
 void print_rev(char *s)
@@ -14,7 +13,7 @@ void print_rev(char *s)
 	while (s[b] != '\0')
 		b++;
 
-	for (a = b - 1; b >= 0; a--)
+	for (a = b - 1; a >= 0; a--)
 	{
 		_putchar(s[a]);
 	}
