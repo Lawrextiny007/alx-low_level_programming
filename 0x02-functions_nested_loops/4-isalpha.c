6333c16c7329de970daec5ada6d9854705e53805
Lawrextiny007 <Lawrencejimmy884@gmail.com>
2023-01-22 03:49:56 -0800

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,7 +10,7 @@ int _isalpha(int c)
 {
 	c = 'a';
 
-	if ((c > 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c > 'a' && c <= 'z') || (c > 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
