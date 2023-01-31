5cbdd4d7c18e76957301227b02b280cf0cc146b1
Lawrextiny007 <Lawrencejimmy884@gmail.com>
2023-01-31 03:22:07 -0800

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - locates a given node of a 
+ * get_nodeint_at_index - locates a given node of a
  * listint_t linked list
  *
- * @head: A pointer to the head 
+ * @head: A pointer to the head
  * of the listint_t list
  *
  * @index: the index of the node to locate - indices start at 0
