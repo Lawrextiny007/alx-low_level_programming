006b1e4d29d480880757588872781fffff819f29
Lawrextiny007 <Lawrencejimmy884@gmail.com>
2023-02-13 02:54:51 -0800

    Advanced task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,14 @@
 /**
  * _isalpha - checks for alphabets
  * @c: character to be checked
- *
+ * @i: integers to be checked
  * Return: 1 if character is a letter,0 otherwise
  */
-int _isalpha(int c)
+int _isalpha(int i, char c)
 {
+	i = 0;
 
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (c > 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
